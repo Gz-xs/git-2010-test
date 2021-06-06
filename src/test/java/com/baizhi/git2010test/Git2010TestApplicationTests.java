0d5b9937c8bc0f815d20de68a9897494a7af0cbb
gz <g15232221494@163.com>
2021-06-06 20:19:13 +0800

    项目基础环境v1.0

---
+++ src/test/java/com/baizhi/git2010test/Git2010TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baizhi.git2010test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Git2010TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
